f74c45104f45122e633cb9c32afce75137754cb4
Atif Ali <aatifali21@gmail.com>
2019-09-24 17:05:16 +0500

    Basics is done

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverMessage = 'Server was not created';
+  serverName: any;
+
+  username: any;
+  buttonStatus = false;
+
+  serverCreated = false;
+  displayDetails = false;
+  log = [];
+
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -18,7 +28,22 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverMessage = 'Server was created!';
+    this.serverMessage = 'Server was created is ' + this.serverName;
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
+
+  resetUserName() {
+    this.username = '';
+  }
+
+  onToggleParagraph() {
+    this.displayDetails = ! this.displayDetails;
+    this.log.push(new Date());
+
   }
 
 }
