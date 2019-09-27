4aff998ca3a0b8d0c523e3cb41b48e27ebff9327
Atif Ali <aatifali21@gmail.com>
2019-09-27 15:30:57 +0500

    fresh copy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { ServersComponent } from './servers/servers.component';
-import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent,
-    ServerComponent
   ],
   imports: [
     BrowserModule,
