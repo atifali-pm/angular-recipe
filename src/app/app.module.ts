f2449e76eae15e7691dd439efd3d6c9abe747e24
atif <atif@monotote.com>
2019-09-29 13:42:09 +0500

    Started up on project management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { ProjectComponent } from './project/project.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProjectComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
